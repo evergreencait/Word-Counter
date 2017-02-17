1e413b1621a91a5eb241e06c1d189e31054acf86
Caitlin Hines <caitlinhines@me.com>
2017-02-17 10:58:41 -0800

    add both tests failing

--- Objects/WordCounter.cs
+++ Objects/WordCounter.cs
@@ -7,14 +7,32 @@ namespace WordCountApp.Objects
   public class RepeatCounter
   {
     private string _wordInput;
+    private string _phraseInput;
+    private int _score;
+    string[] _phraseArray;
 
-    public RepeatCounter (string wordInput)
+    public RepeatCounter (string wordInput , string phraseinput)
     {
       _wordInput = wordInput;
+      _phraseArray = _phraseInput.Split(' ');
     }
     public string GetWord()
     {
       return _wordInput;
+    }
+
+    public int CountRepeats()
+    {
+      _score = 0;
+
+      for (int i = 0; i <= _phraseArray.Length; i++)
+      {
+        if (_wordInput == _phraseArray[i])
+        {
+           _score +=1;
+        }
+      }
+      return _score;
     }
   }
 }
--- Tests/WordCounterTest.cs
+++ Tests/WordCounterTest.cs
@@ -7,11 +7,20 @@ namespace WordCountApp.Objects
   public class WordCountTest
   {
     [Fact]
-    public void RepeatCounter_ReturnWord_word()
+    public void GetWord_ReturnWord_word()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter("cat");
+      RepeatCounter testRepeatCounter = new RepeatCounter("cat", "cat");
       string expected = "cat";
       string output = testRepeatCounter.GetWord();
+      Assert.Equal(expected, output);
+    }
+
+    [Fact]
+    public void CountRepeats_ReturnOne_one()
+    {
+      RepeatCounter testRepeatCounter = new RepeatCounter("dog", "dog");
+      int expected = 1;
+      int output = testRepeatCounter.CountRepeats();
       Assert.Equal(expected, output);
     }
   }
