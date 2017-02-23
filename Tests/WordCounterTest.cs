5fff532930d1a7fcee31d6a4c8c52f9183809f94
Caitlin Hines <caitlinhines@me.com>
2017-02-22 18:45:46 -0800

    add test for single instance of a word in the sentence

--- Tests/WordCounterTest.cs
+++ Tests/WordCounterTest.cs
@@ -6,26 +6,35 @@ namespace WordCounterApp.Objects
 {
   public class RepeatCounterTest
   {
+    // [Fact]
+    // public void RepeatCounter_ReturnAllInputs_allInputs()
+    // {
+    //   RepeatCounter testRepeatCounter = new RepeatCounter("cat", "cat");
+    //   List<string> expected = new List<string>{"cat", "cat"};
+    //   List<string> output = testRepeatCounter.GetAllInputs();
+    //   Assert.Equal(expected, output);
+    // }
+    //
+    // [Fact]
+    // public void RepeatCounter_ReturnIfMatch_MatchConfirmation()
+    // {
+    //   RepeatCounter testRepeatCounter = new RepeatCounter("rabbit", "rabbit");
+    //   List<string> expected = new List<string>{"rabbit", "Your word was in the sentence"};
+    //   List<string> output = testRepeatCounter.ReturnSentence();
+    //   Assert.Equal(expected, output);
+    // }
+
     [Fact]
-    public void RepeatCounter_ReturnAllInputs_allInputs()
+    public void CountRepeats_ReturnIfOne_Score()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter("cat", "cat");
-      List<string> expected = new List<string>{"cat", "cat"};
-      List<string> output = testRepeatCounter.GetAllInputs();
+      RepeatCounter testRepeatCounter = new RepeatCounter("This is my dog", "dog");
+      int expected = 1;
+      int output = testRepeatCounter.CountRepeats();
       Assert.Equal(expected, output);
     }
 
     [Fact]
-    public void RepeatCounter_ReturnIfMatch_MatchConfirmation()
-    {
-      RepeatCounter testRepeatCounter = new RepeatCounter("rabbit", "rabbit");
-      List<string> expected = new List<string>{"rabbit", "Your word was in the sentence"};
-      List<string> output = testRepeatCounter.ReturnSentence();
-      Assert.Equal(expected, output);
-    }
-
-    [Fact]
-    public void RepeatCounter_ReturnScore_Score()
+    public void CountRepeats_ReturnScore_Score()
     {
       RepeatCounter testRepeatCounter = new RepeatCounter("This is my dog and his dog", "dog");
       int expected = 2;
@@ -34,7 +43,7 @@ namespace WordCounterApp.Objects
     }
 
     [Fact]
-  public void RepeatCounter_ReturnCaseInsensitive_Score()
+  public void CountRepeatss_ReturnCaseInsensitive_Score()
   {
     RepeatCounter testRepeatCounter = new RepeatCounter("This is my dog", "Dog");
     int expected = 1;
